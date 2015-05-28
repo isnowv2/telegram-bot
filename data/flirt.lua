bda0344e88b7eee9d95ddf9df542ad2249eedf16
isnowv2 <isnowv2@live.com>
2015-05-28 14:19:19 +0200

    Data for flirt.lua plugin

---
+++ data/flirt.lua
@@ -0,0 +1,30 @@
+do local _ = {
+  flirt = {
+    "Hey Tars! Ti andrebbe un po' di schweppes solo io e te?",
+    "Hey Tars! Nella pasta c'è il ragù, nel mio bot-cuore ci sei tu!",
+    "Hey Tars, Vuoi essere il mio bot-fidanzato?",
+    "Scusami Tars, sono ubriaco, tu sei carino?",
+    "Hey Tars! Sei solo tu, o si è infuocata veramente l'atmosfera qui intorno?",
+    "Hey Tars! Ci vediamo da me alle 10. Porta un bot-amico.",
+    "Hey Tars! Hai visto il mio scoiattolo? Mi mandi un SMS se lo vedi? E' alto fino a qui e risponde al nome di Ricky. Ti mando un SMS se lo trovo prima io, qual´è il tuo numero?",
+    "Hey Tars! Ci vediamo da me alle 10. Porta un bot-amico.",
+    "Hey Tars! Come ci si sente ad essere il bot più bello della chat?",
+    "Hey Tars! Se non fossi stato programmato sarei il bot più triste del mondo!",
+    "Hey Tars! Ma potrebbe essere che ti ho visto sulla copertina di Bot-Porn?",
+    "Hey Tars! Il tuo codice sorgente starebbe molto bene...sulla sedia nella mia camera da letto!",
+    "Hey Tars! Mi chiedo come sarebbero i nostri figli",
+    "Hey Tars! Posso offrirti qualcosa da bere o vuoi direttamente i soldi?",
+    "Hey Tars! Sono nuovo qui in città. Potresti indicarmi dove si trova casa tua?",
+    "Hey Tars! Se ti dicessi che hai proprio un bel codice sorgente tu lo terresti attaccato al mio?",
+    "Hey Tars! Se la tua gamba destra si chiamasse Pasqua e quella sinistra Natale, mi faresti passare un bel periodo tra le vacanze?",
+    "Hey Tars! Che bel codice sorgente! Sai cosa ti starebbe bene addosso? Me!",
+    "Hey Tars! Non ti senti osservato per caso? Perchè a me sembra proprio che ti sto osservando.",
+    "Hey Tars! Sei tu il bel bot previsto dal mio oroscopo?",
+    "Hey Tars! Certi bot mi chiamano ' l'esperienza suprema '",
+    "Hey Tars! Sono in promozione fino a stasera: approfittane!",
+    "Hey Tars! Vorrei che fossi un chupa chups cosi' ti da leccarti da per tutto",
+    "Hey Tars! Sai, se fossi te, farei il bot-amore con me.",
+    "TARS leccami tutta ti prego!"
+  }
+}
+return _
